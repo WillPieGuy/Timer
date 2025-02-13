83932376200e43aa7cacc3d5be3b2fcddb042e79
Will Pie <will@willpie.com>
2025-02-12 21:59:58 -0600

    git commit -m "Update Home.tsx"

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -52,7 +52,7 @@ export default function Home() {
         )}
       </div>
 
-      {showQuickTimer && <QuickTimer />} {/* Conditionally render QuickTimer */}
+      {showQuickTimer && <QuickTimer minutes={5} />} {/* Pass a default value for minutes */}
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {timers.map((timer) => (
