43d481544dcbba0f3d1ec80686e065ace4aaa59f
Will Pie <will@willpie.com>
2025-02-19 16:06:19 -0600

    git commit -m "Update Home.tsx"

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -43,6 +43,7 @@ export default function Home() {
             </Link>
           ) : (
             <Link
+            onClick={() => window.dispatchEvent(new CustomEvent('open-auth-modal'))}
               to="/login"
               className="flex items-center gap-2 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors"
             >
